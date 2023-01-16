b16205eb95ff954e1a903bba12bc200674de772f
Mathew Polzin <matt.polzin@gmail.com>
2023-01-16 15:29:29 -0600

    Add control_curses_ticker example nix flake.

--- examples/control_curses_ticker/flake.nix
+++ examples/control_curses_ticker/flake.nix
@@ -6,17 +6,47 @@
     idris2 = {
       url = "github:idris-lang/idris2/main";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
     ncurses-idris = {
       url = "github:mattpolzin/ncurses-idris/nix-experiment";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
       inputs.idris2.follows = "idris2";
     };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, flake-utils, idris2, ncurses-idris }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) stdenv;
+        idris2' = idris2.defaultPackage.${system};
+        ncurses-idris' = ncurses-idris.packages.${system}.default;
+        inherit (builtins) concatStringsSep;
+    in {
+      packages.default = stdenv.mkDerivation rec {
+        name = "control-curses-ticker";
+        version = "0.0.1";
+        src = ./.;
+        idrisPackages = [ ncurses-idris' ] ++ ncurses-idris'.idrisPackages;
+        buildInputs = [
+          idris2'
+        ] ++ idrisPackages;
 
-  };
+        IDRIS2="${idris2'}/bin/idris2";
+        IDRIS2_PACKAGE_PATH = concatStringsSep ":" (map (p: "${p}/idris2-${idris2'.version}") idrisPackages);
+
+        buildPhase = ''
+          make clean
+          make app
+        '';
+
+        installPhase = ''
+          rm ./build/exec/control_curses_ticker_app/compileChez
+          rm ./build/exec/control_curses_ticker_app/control_curses_ticker.ss
+          mkdir -p $out/bin/control_curses_ticker_app
+          install ./build/exec/control_curses_ticker_app/* $out/bin/control_curses_ticker_app/
+          install ./build/exec/control_curses_ticker $out/bin/
+        '';
+      };
+    }
+  );
 }
